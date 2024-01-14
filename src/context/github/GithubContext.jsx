06a8a66cabf048468a3fea116883a66027e31ccc
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-14 15:08:18 -0800

    user profile and repos design

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -10,12 +10,13 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: [],
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //   Get search results
+  //   Get search us
   const searchUsers = async (text) => {
     setLoading();
 
@@ -59,6 +60,32 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  // Get user repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    });
+  };
+
   // Clear users
   const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
 
@@ -71,9 +98,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         user: state.user,
         loading: state.loading,
+        repos: state.repos,
         searchUsers,
         getUser,
         clearUsers,
+        getUserRepos,
       }}
     >
       {children}
